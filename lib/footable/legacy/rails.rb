3cfac095312f24062383dc94858e8841a19069ef
Toshiyuki HINA <thina@yourinventit.com>
2019-03-08 11:05:02 +0900

    initial commit

---
+++ lib/footable/legacy/rails.rb
@@ -0,0 +1,10 @@
+require "footable/legacy/rails/version"
+
+module Footable
+  module Legacy
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/footable/legacy/rails/version.rb
@@ -0,0 +1,7 @@
+module Footable
+  module Legacy
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
