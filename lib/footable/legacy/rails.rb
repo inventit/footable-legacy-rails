8c751e523ca633e33ba3c4d143d4f89d7b6fb4ad
Toshiyuki HINA <thina@yourinventit.com>
2019-03-08 12:25:17 +0900

    Add initializer and add precompile list

--- lib/footable/legacy/rails.rb
+++ lib/footable/legacy/rails.rb
@@ -4,6 +4,9 @@ module Footable
   module Legacy
     module Rails
       class Engine < ::Rails::Engine
+        initializer 'footable-legacy.assets.precompile' do |app|
+          app.config.assets.precompile << %r(footable\.(?:eot|svg|ttf|woff)$)
+        end
       end
     end
   end
